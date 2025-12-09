dacf32a9b9ed9a27f12a324e19a509537d5eb9e8
Dani Mayfield <danimmayfield@gmail.com>
2025-12-09 16:00:22 -0500

    chore: add vercel's analytics package to the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Box } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/next';
 import { Providers } from './providers';
 import Footer from '@/features/layout/Footer';
 import Nav from '@/features/layout/Nav';
@@ -17,6 +18,7 @@ export default function RootLayout({
           <Nav />
           <Box as='main' minH='80vh'>
             {children}
+            <Analytics />
           </Box>
           <Footer />
         </Providers>
